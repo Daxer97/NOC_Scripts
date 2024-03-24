7c050e4bd9c98e56dc2ee6268bcd658a81d6e4e0
Alessandro Da Silva <73776698+Daxer97@users.noreply.github.com>
2024-03-24 15:57:27 +0100

    Add files via upload

---
+++ SMF_SNMP_RISK.py
@@ -0,0 +1,77 @@
+def find_links_SMF():
+
+	arr = []
+
+	# Open the file containing all json links in sparkle
+	with open('RACCORDI.json', 'r') as file:
+		racc = json.load(file)
+		racc = racc['rows'] # List of dictionary
+		# print(racc)
+
+		# Create a new dictionary containing  filtered(RACCORDI.josn(parent, name)
+		# print(type(racc))
+
+		for items in racc:
+			for i in items.keys():
+				# print(item.keys())
+				if i == 'parent':
+					#print(items[i])
+					arr.append(items[i])
+
+		result = misc.find_links(arr)
+
+		print(result)
+		misc.find_dsc(racc,result)
+		# print(type(result))
+		# print(len(result))
+
+		# for i in result:
+		# 	print('result')
+		# 	print(i)
+	return
+
+
+#-------------------------------------------------------------------------------------
+
+# Return parent values of the SMF at risk
+
+def find_links(x):
+	y = []
+	# For loop to check if the value is 0 
+	# Than check the x.index(0) +3 == 0
+		# recursion with  index value to next occurence of 0
+
+	for index, items in enumerate(x):
+	    if items == '0':
+	    	# print(index)
+	    	#print(len(x) - index)
+	    	if ((len(x) - 1) - index) > 3:
+	    		# print('enr')
+	    		if x[index + 3] == '0':
+	    			#print('HERE')
+	    			y.append(x[index + 1])
+	    			y.append(x[index + 2])
+	return y
+
+#-------------------------------------------------------------------------------------
+
+# Find the description of associated vulnerable links for SMF
+
+def find_dsc(x, lis):
+	a = []
+
+	for index, items in enumerate(x):
+		for y in lis:
+			if items['parent'] == y:
+				print(items['name'])
+				a.append(items['name'])
+
+	return
+
+
+#-------------------------------------------------------------------------------------
+# Main function used in order to troubleshoot the scai() class
+
+if __name__ == "__main__":
+
+	find_links_SMF()
